0f0e6464d8448e7cb157099f33178c6ff1c4ebe4
Chaco van der Sijp <vandersijp@gmail.com>
2016-10-04 18:44:41 +0100

    Add files via upload

--- appModule.js
+++ appModule.js
@@ -3,7 +3,7 @@
 //  button to close the sideNav
 //  <md-button ng-click="close()" class="md-primary" hide-gt-md="">close</md-button>
 
-console.log("Built 26");
+console.log("Built 27");
 
 String.repeat = function(string, num) {
     return new Array(parseInt(num) + 1).join(string);
