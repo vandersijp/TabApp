6320d9526aa212b9d801f5ab06631d235e923a40
Chaco van der Sijp <vandersijp@gmail.com>
2016-10-05 15:26:37 +0100

    Add files via upload

--- appModule.js
+++ appModule.js
@@ -3,7 +3,7 @@
 //  button to close the sideNav
 //  <md-button ng-click="close()" class="md-primary" hide-gt-md="">close</md-button>
 
-console.log("App 20161005.16");
+console.log("App 20161005.17");
 
 String.repeat = function(string, num) {
     return new Array(parseInt(num) + 1).join(string);
@@ -46,8 +46,8 @@ function getMessageDefaults() {
 }
 
 window.appProperties = getAppProperties();
-var window.messageDefaults.defaults = {};
-window.messageDefaults.defaults = getMessageDefaults();
+//var window.appProperties.defaults = {};
+window.appProperties.defaults = getMessageDefaults();
 
 var app = angular.module('app', ['ngMaterial', 'ngAnimate', 'firebase', 'ngSanitize', 'ngMessages', 'alsContact', 'alsAccess', 'alsIcon', 'alsList', 'alsFigure', 'alsTab'])
 
