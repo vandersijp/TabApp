2eb45fdc673232e7ef65a7694baec558e08349ea
Chaco van der Sijp <vandersijp@gmail.com>
2017-04-10 13:48:36 +0100

    Add files via upload

--- appModule.js
+++ appModule.js
@@ -191,7 +191,7 @@ app.config(function($mdThemingProvider, $sceDelegateProvider) {
         "asklearnshare-com": {},
         "mafia-eu": {
             "primary": "red",
-            "accent": "amber"
+            "accent": "blue"
           },
         "neverturkey-eu": {
             "primary": "red",
