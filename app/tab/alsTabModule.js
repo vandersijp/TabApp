aa0d90aafa90dd23ee2dafe7941022f25b1a285b
Chaco van der Sijp <vandersijp@gmail.com>
2018-02-28 11:21:58 +0000

    Add files via upload

--- app/tab/alsTabModule.js
+++ app/tab/alsTabModule.js
@@ -1,7 +1,7 @@
 /* (C) 2016 Ask Learn Share Ltd */
-console.log("Tab 12");
+console.log("Tab 13");
 
-var alsTab = angular.module("alsTab", ['ngMaterial', 'ngAnimate', 'ngSanitize', 'alsIcon', 'alsList', 'alsCard', 'alsText', 'alsFigure']);
+var alsTab = angular.module("alsTab", ['ngMaterial', 'ngAnimate', 'ngSanitize', 'alsIcon', 'alsList', 'alsCard', 'alsText', 'alsFigure', 'alsTemplate']);
 
 alsTab.directive('alsTabContent', function() {
     return {
