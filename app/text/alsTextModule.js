a54a6ea420521bf98e8d4a399a1828c46e9b4ed5
Chaco van der Sijp <vandersijp@gmail.com>
2018-08-18 13:38:22 +0100

    Add files via upload

--- app/text/alsTextModule.js
+++ app/text/alsTextModule.js
@@ -1,5 +1,5 @@
 /* (C) 2016 Ask Learn Share Ltd */
-console.log("Text 6");
+console.log("Text 7");
 
 var alsText = angular.module("alsText", ['ngMaterial', 'ngAnimate', 'ngSanitize']);
 
@@ -27,7 +27,7 @@ alsText.directive('alsTextSection', function() {
     return {
         restrict: 'E',
         scope: {
-            section: '=';
+            section: '=',
             filter: '='
         },
         templateUrl: window.x.app + 'text/alsTextSection.html'
