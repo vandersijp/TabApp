d6b0e82a93fbe4f939c758a0b0b1d0e1c7963252
Chaco van der Sijp <vandersijp@gmail.com>
2018-08-25 15:01:20 +0100

    Add files via upload

--- app/text/alsTextModule.js
+++ app/text/alsTextModule.js
@@ -1,52 +1,57 @@
 /* (C) 2016 Ask Learn Share Ltd */
-console.log("Text 22");
+console.log("Text 23");
 
 var alsText = angular.module("alsText", ['ngMaterial', 'ngAnimate', 'ngSanitize']);
 
 alsText.directive('alsTextParagraphs', function() {
-    return {
-        restrict: 'E',
-        scope: {
-            section: '='
-        },
-        templateUrl: window.x.app + 'text/alsTextParagraphs.html'
-    }
+  return {
+    restrict: 'E',
+    scope: {
+      section: '='
+    },
+    templateUrl: window.x.app + 'text/alsTextParagraphs.html'
+  }
 });
 
 alsText.directive('alsTextHeader', function() {
-    return {
-        restrict: 'E',
-        scope: {
-            section: '='
-        },
-        templateUrl: window.x.app + 'text/alsTextHeader.html'
-    }
+  return {
+    restrict: 'E',
+    scope: {
+      section: '='
+    },
+    templateUrl: window.x.app + 'text/alsTextHeader.html'
+  }
 });
 
 alsText.directive('alsTextSection', function() {
-    return {
-        restrict: 'E',
-        scope: {
-            section: '=',
-            filter: '='
-            /*
-            bitmask
-            1 = include only titles
-            2 = include only paragraphs
-            4 = include only sub section or sections
-            */
-        },
-        templateUrl: window.x.app + 'text/alsTextSection.html'
-    }
+  return {
+    restrict: 'E',
+    scope: {
+      section: '=',
+      filter: '=?'
+      /*
+      bitmask
+      1 = include only titles
+      2 = include only paragraphs
+      4 = include only sub section or sections
+      */
+    },
+    controller: function($scope) {
+      // check if it was defined.  If not - set a default
+      // from https://stackoverflow.com/questions/18784520/angular-directive-with-default-options
+      $scope.filter = angular.isDefined($scope.filter) ? $scope.filter : ["header", "title", "lead", "paragraphs", "footer"];
+    },
+    templateUrl: window.x.app + 'text/alsTextSection.html'
+  }
 });
 
 alsText.directive('alsTextPlaylist', function() {
-    return {
-        restrict: 'E',
-        scope: {
-            playlist: '=',
-            cuts: '='
-        },
-        templateUrl: window.x.app + 'text/alsTextPlaylist.html'
-    }
+  return {
+    restrict: 'E',
+    scope: {
+      playlist: '=',
+      cuts: '='
+    },
+    templateUrl: window.x.app + 'text/alsTextPlaylist.html'
+  }
 });
