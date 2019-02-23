e3f82fd34403a693d9594b678aa0213b5fa7e9e9
Chaco van der Sijp <vandersijp@gmail.com>
2019-02-23 15:32:41 +0000

    Add files via upload

--- js/als.js
+++ js/als.js
@@ -1,16 +1,15 @@
 /* (C) Ask Learn Share Ltd */
-// version 20190222
+// version 20190223
 //
-// triggers for the animations
+var alsParallaxElements = document.getElementsByClassName('als-parallax');
+var alsParallaxElementsVisibilities = [];
 var alsPreviousScrollPosition = window.pageYOffset;
+//
 window.onscroll = function(ev) {
-  // work in progress to create a parallax effect on the banners
-  //console.log(window.pageYOffset + '|' + window.innerHeight);
-  //var x = document.getElementsByClassName("parallax");
-  //x.style.transform = "rotateX(45deg)";
-  //if (window.scrollHeight - window.scrollTop === window.clientHeight) {};
+  //
+  // triggers for the animations
   var alsCurrentScrollPosition = window.pageYOffset;
-  // to cover negative values in Safari
+  // to prevent bouncing in case of negative values in Safari
   if (alsCurrentScrollPosition <= 0) {
     alsSwitchClassOn("als-navbar");
     alsSwitchClassOn("als-scrabble");
@@ -30,6 +29,16 @@ window.onscroll = function(ev) {
     }
   }
   alsPreviousScrollPosition = alsCurrentScrollPosition;
+  //
+  // parallax
+  var i;
+  for (i = 0; i < alsParallaxElements.length; i++) {
+    alsParallaxElementsVisibilities[i] = (window.innerHeight - alsParallaxElements[i].getBoundingClientRect().top) / window.innerHeight;
+    if (alsParallaxElementsVisibilities[i] > 0 && alsParallaxElementsVisibilities[i] < 1) {
+      alsParallaxElements[i].style.transform = "rotate(" + alsParallaxElementsVisibilities[i] * 3 + "deg)" + " " + "scale(" + (1.5 + (1 - alsParallaxElementsVisibilities[i]) / 20) + ")";
+      //console.log(i + " | " + alsParallaxElementsVisibilities[i]);
+    }
+  }
 }
 // move to the top of the screen
 function alsScrolTo(position) {
